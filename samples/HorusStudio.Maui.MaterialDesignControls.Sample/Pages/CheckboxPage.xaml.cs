e06b99111954cd2cd745e51383dac71789ab1e47
Andres Aldana <andres.aldana@horus.com.uy>
2024-05-17 13:33:56 -0500

    create sample pages for checkboxes

---
+++ samples/HorusStudio.Maui.MaterialDesignControls.Sample/Pages/CheckboxPage.xaml.cs
@@ -0,0 +1,11 @@
+using HorusStudio.Maui.MaterialDesignControls.Sample.ViewModels;
+
+namespace HorusStudio.Maui.MaterialDesignControls.Sample.Pages;
+
+public partial class CheckboxPage :  BaseContentPage<CheckboxViewModel>
+{
+    public CheckboxPage(CheckboxViewModel viewModel) : base(viewModel)
+    {
+        InitializeComponent();
+    }
+}
---
+++ samples/HorusStudio.Maui.MaterialDesignControls.Sample/ViewModels/CheckboxViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace HorusStudio.Maui.MaterialDesignControls.Sample.ViewModels;
+public partial class CheckboxViewModel : BaseViewModel
+{
+    #region Attributes & Properties
+
+    public override string Title => "Checkboxes";
+
+    #endregion
+
+    public CheckboxViewModel()
+    {
+        Subtitle = "Checkboxes let users select one or more items from a list, or turn an item on or off";
+    }
+}
