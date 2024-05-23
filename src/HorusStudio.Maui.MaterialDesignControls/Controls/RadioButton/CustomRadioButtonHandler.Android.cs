0c96ec287a8f33c735325dcd01a2b4b1cbc70283
Andres Aldana <andres.aldana@horus.com.uy>
2024-05-23 10:48:13 -0500

    chore: refact

--- src/HorusStudio.Maui.MaterialDesignControls/Controls/RadioButton/CustomRadioButtonHandler.Android.cs
+++ src/HorusStudio.Maui.MaterialDesignControls/Controls/RadioButton/CustomRadioButtonHandler.Android.cs
@@ -1,5 +1,5 @@
-﻿using Microsoft.Maui.Controls.Compatibility.Platform.Android;
-using Microsoft.Maui.Handlers;
+﻿using Microsoft.Maui.Handlers;
+using Microsoft.Maui.Platform;
 
 namespace HorusStudio.Maui.MaterialDesignControls;
 
@@ -7,14 +7,10 @@ partial class CustomRadioButtonHandler
 {
     public new static void MapStrokeColor(IRadioButtonHandler handler, IRadioButton radioButton)
     {
-        if (radioButton is CustomRadioButton customRadioButton && customRadioButton.StrokeColor != null)
+        if (radioButton is CustomRadioButton customRadioButton && customRadioButton.StrokeColor != null && handler.PlatformView != null && handler.PlatformView is Android.Widget.RadioButton androidRadioButton)
         {
-            var strokeColor = customRadioButton.StrokeColor.ToAndroid();
-           
-            if (handler.PlatformView != null && handler.PlatformView is Android.Widget.RadioButton androidRadioButton)
-            {
-                androidRadioButton.ButtonTintList = Android.Content.Res.ColorStateList.ValueOf(strokeColor);
-            }
+            var strokeColor = customRadioButton.StrokeColor.ToPlatform();
+            androidRadioButton.ButtonTintList = Android.Content.Res.ColorStateList.ValueOf(strokeColor);
         }
     }
 }
