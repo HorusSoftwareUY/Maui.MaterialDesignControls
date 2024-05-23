fa7360ed3da1c5b87d4c80a06a4b148cf3a6a4dc
Andres Aldana <andres.aldana@horus.com.uy>
2024-05-23 11:35:52 -0500

    chore: refactor

--- src/HorusStudio.Maui.MaterialDesignControls/Controls/RadioButton/CustomRadioButtonHandler.Windows.cs
+++
@@ -1,7 +0,0 @@
-﻿using Microsoft.Maui.Handlers;
-
-namespace HorusStudio.Maui.MaterialDesignControls;
-partial class CustomRadioButtonHandler
-{
-    public new static void MapStrokeColor(IRadioButtonHandler handler, IRadioButton radioButton){ }
-}
