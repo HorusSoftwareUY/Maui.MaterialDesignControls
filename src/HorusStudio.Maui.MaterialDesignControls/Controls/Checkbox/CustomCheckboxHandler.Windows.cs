19b6b74ac1029ea486afc057a42558c82fdf4ceb
Andres Aldana <andres.aldana@horus.com.uy>
2024-05-23 11:35:27 -0500

    fix: isue with andorid checkbox color

--- src/HorusStudio.Maui.MaterialDesignControls/Controls/Checkbox/CustomCheckboxHandler.Windows.cs
+++
@@ -1,7 +0,0 @@
-﻿using Microsoft.Maui.Handlers;
-
-namespace HorusStudio.Maui.MaterialDesignControls;
-partial class CustomCheckboxHandler
-{
-    public new static void MapForeground(ICheckBoxHandler handler, ICheckBox check) { }
-}
