cc1a2387952251df8d4810b33c5c8bd41be3772e
Andres Aldana <andres.aldana@horus.com.uy>
2024-08-01 06:54:35 -0500

    chore: refactor

--- src/HorusStudio.Maui.MaterialDesignControls/Controls/DatePicker/MaterialDatePicker.cs
+++ src/HorusStudio.Maui.MaterialDesignControls/Controls/DatePicker/MaterialDatePicker.cs
@@ -10,9 +10,7 @@ public class MaterialDatePicker : MaterialInputBase
 
     #region Attributes
 
-    private readonly static Color DefaultTextColor = new AppThemeBindingExtension { Light = MaterialLightTheme.OnSurface, Dark = MaterialLightTheme.OnSurface }.GetValueForCurrentTheme<Color>();
     private readonly static double DefaultCharacterSpacing = MaterialFontTracking.BodyLarge;
-    private readonly static Color DefaultCursorColor = new AppThemeBindingExtension { Light = MaterialLightTheme.Primary, Dark = MaterialLightTheme.Primary }.GetValueForCurrentTheme<Color>();
 
     #endregion Attributes
 
