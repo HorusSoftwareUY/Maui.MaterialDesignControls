99dd38cca3bd05c35daf483ecdb95bb8d7b41fb6
Agustin Bonilla <agustin.bonilla@horus.com.uy>
2025-06-27 14:56:54 -0300

    Update KeyboardExtensions.Android.cs

--- src/HorusStudio.Maui.MaterialDesignControls/Extensions/Keyboard/KeyboardExtensions.Android.cs
+++ src/HorusStudio.Maui.MaterialDesignControls/Extensions/Keyboard/KeyboardExtensions.Android.cs
@@ -2,21 +2,18 @@
 
 static class KeyboardExtensions
 {
-    public static void HideKeyboard(this Activity activity, Views.View? view) => activity.RunOnUiThread(() =>
+    public static void HideKeyboard(this Activity activity, Views.View? view) => activity.SafeRunOnUiThread(() =>
     {
-        activity.SafeRunOnUiThread(() =>
+        var context = activity.ApplicationContext;
+        if (context != null)
         {
-            var context = activity.ApplicationContext;
-            if (context != null)
+            var systemService = context.GetSystemService(Content.Context.InputMethodService);
+            if (view != null
+                && systemService != null
+                && systemService is Views.InputMethods.InputMethodManager inputMethodManager)
             {
-                var systemService = context.GetSystemService(Content.Context.InputMethodService);
-                if (view != null
-                    && systemService != null
-                    && systemService is Views.InputMethods.InputMethodManager inputMethodManager)
-                {
-                    inputMethodManager?.HideSoftInputFromWindow(view.WindowToken, Views.InputMethods.HideSoftInputFlags.None);
-                }
+                inputMethodManager?.HideSoftInputFromWindow(view.WindowToken, Views.InputMethods.HideSoftInputFlags.None);
             }
-        });
+        }
     });
 }
