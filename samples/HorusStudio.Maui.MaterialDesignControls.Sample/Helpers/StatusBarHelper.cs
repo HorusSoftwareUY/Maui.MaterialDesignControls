1fe281399374f627fa09b0b433d5607c3cf6beda
Daniel Quintero Henao <danielquintero@technium.dev>
2025-10-13 18:14:02 -0500

    Add StatusBarHelper for theme-based status bar color

---
+++ samples/HorusStudio.Maui.MaterialDesignControls.Sample/Helpers/StatusBarHelper.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Maui.Platform;
+
+namespace HorusStudio.Maui.MaterialDesignControls.Sample.Helpers
+{
+    internal class StatusBarHelper
+    {
+        internal static void SetStatusBarColor(Color color, bool darkStatusBarTint)
+        {
+#if ANDROID
+            var window = Platform.CurrentActivity?.Window;
+
+            if (window == null) return;
+
+            window.SetStatusBarColor(color.ToPlatform());
+
+            var flag = Android.Views.SystemUiFlags.LightStatusBar;
+
+            if (darkStatusBarTint)
+                window.DecorView.SystemUiVisibility |= (Android.Views.StatusBarVisibility)flag;
+            else
+                window.DecorView.SystemUiVisibility &= ~(Android.Views.StatusBarVisibility)flag;
+#endif
+
+#if IOS
+            var uiColor = color.ToPlatform();
+
+            var currentView = UIKit.UIApplication.SharedApplication.KeyWindow;
+            if (currentView == null) return;
+
+            currentView.BackgroundColor = uiColor;
+
+            UIKit.UIApplication.SharedApplication.StatusBarStyle =
+                darkStatusBarTint ? UIKit.UIStatusBarStyle.DarkContent : UIKit.UIStatusBarStyle.LightContent;
+#endif
+        }
+    }
+}
--- samples/HorusStudio.Maui.MaterialDesignControls.Sample/ViewModels/AppearanceViewModel.cs
+++ samples/HorusStudio.Maui.MaterialDesignControls.Sample/ViewModels/AppearanceViewModel.cs
@@ -1,3 +1,4 @@
+using HorusStudio.Maui.MaterialDesignControls.Sample.Helpers;
 using Microsoft.Toolkit.Mvvm.ComponentModel;
 using Microsoft.Toolkit.Mvvm.Input;
 
@@ -29,7 +30,13 @@ namespace HorusStudio.Maui.MaterialDesignControls.Sample.ViewModels
         {
             SelectedThemeMode = theme;
 
-            Application.Current?.UserAppTheme = theme;
+            if (Application.Current != null)
+                Application.Current.UserAppTheme = theme;
+
+            var currentTheme = Application.Current?.RequestedTheme;
+            var isDark = currentTheme == AppTheme.Dark;
+
+            StatusBarHelper.SetStatusBarColor(isDark ? MaterialDarkTheme.Surface : MaterialLightTheme.Surface, !isDark);
         }
     }
 }
